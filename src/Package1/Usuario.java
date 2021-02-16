7581a7b71323230f42e1129b3b8d783a1bbfec38
Angel <Angel@DESKTOP-5DTAT88>
2021-02-16 16:42:28 -0600

    chems_xd

---
+++ src/Package1/Usuario.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Usuario {
+
+    /**
+     * Default constructor
+     */
+    public Usuario() {
+    }
+
+    /**
+     * 
+     */
+    private String user;
+
+    /**
+     * 
+     */
+    private String psw;
+
+    /**
+     * 
+     */
+    private String correoElectronico1;
+
+}
